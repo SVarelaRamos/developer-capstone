77410a8901d8be8fa456966d2ef421de943ee9fd
Sergio Varela Ramos <s.varelaramos@gmail.com>
2024-01-06 20:11:28 +0100

    Namechange Homepage

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import "./App.css";
 import Footer from "./Footer";
 import Header from "./Header";
-import Main from "./Main";
+import HomePage from "./HomePage";
 
 function App() {
   return (
     <>
       <Header />
-      <Main />
+      <HomePage />
       <Footer />
     </>
   );
